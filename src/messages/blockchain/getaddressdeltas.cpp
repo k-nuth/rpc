c32e9f428258eaee20e87c9ca3e1588b14c6c5b7
hanchon <guillermo@bitprim.org>
2017-11-29 15:56:14 -0300

    Getaddressdeltas improvements

--- src/messages/blockchain/getaddressdeltas.cpp
+++ src/messages/blockchain/getaddressdeltas.cpp
@@ -26,6 +26,9 @@ namespace bitprim {
 
 bool json_in_getaddressdeltas(nlohmann::json const& json_object, std::vector<std::string>& payment_address, size_t& start_height, size_t& end_height, bool& include_chain_info)
 {
+    // Example:
+    // curl --user bitcoin:local321 --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getaddressdeltas", "params": [{"addresses": ["mvBZZ2s7yCQmRN7FoYkHDXdXpWqtFydXUg"], "start":1, "end":1192283}] }' -H 'content-type: text/plain;' http://127.0.0.1:8332/
+
     if (json_object["params"].size() == 0)
         return false;
 
@@ -36,13 +39,13 @@ bool json_in_getaddressdeltas(nlohmann::json const& json_object, std::vector<std
     auto temp = json_object["params"][0];
     if (temp.is_object()){
         if (!temp["start"].is_null()){
-            start_height = temp["start"][0];
+            start_height = temp["start"];
         }
         if (!temp["end"].is_null()){
-            end_height = temp["end"][0];
+            end_height = temp["end"];
         }
         if(!temp["chainInfo"].is_null()){
-            include_chain_info = temp["chainInfo"][0];
+            include_chain_info = temp["chainInfo"];
         }
 
         for (const auto & addr : temp["addresses"]){
@@ -64,85 +67,72 @@ bool getaddressdeltas (nlohmann::json& json_object, int& error, std::string& err
         chain.fetch_history(address, INT_MAX, 0, [&](const libbitcoin::code &ec,
                                                      libbitcoin::chain::history_compact::list history_compact_list) {
             if (ec == libbitcoin::error::success) {
-                auto const history_list = expand(history_compact_list);
-                for (const auto & history : history_list) {
-                    if (history.spend.hash() == libbitcoin::null_hash && history.output_height >= start_height &&
-                        history.output_height <= end_height) {
-                        //Es outpoint
-                        // Need to look for the txns because the position the block is required
-                        //                                    json_object[i]["txid"] = history.output.hash();
-                        //                                    json_object[i]["index"] = history.output.index();
-                        //                                    json_object[i]["address"] = address.encoded();
-                        //                                    json_object[i]["height"] = history.spend_height;
-                        //                                    json_object[i]["satoshis"] = history.value;
-                        //                                    ++i;
-                        {
-                            boost::latch latch2(2);
-                            chain.fetch_transaction(history.output.hash(), false,
-                                                    [&](const libbitcoin::code &ec,
-                                                        libbitcoin::transaction_const_ptr tx_ptr, size_t index,
-                                                        size_t height) {
-                                                        if (ec == libbitcoin::error::success) {
-                                                            if (height >= start_height && height <= end_height) {
-                                                                json_object[i]["txid"] = libbitcoin::encode_hash(
-                                                                        history.output.hash());
-                                                                json_object[i]["index"] = history.output.index();
-                                                                json_object[i]["address"] = address.encoded();
-                                                                json_object[i]["blockindex"] = index;
-                                                                json_object[i]["height"] = height;
-                                                                json_object[i]["satoshis"] = std::to_string(
-                                                                        history.value);
-                                                                ++i;
-                                                            }
-                                                        } else {
-                                                            std::cout << "Tx not found" << std::endl;
+                for(const auto & history : history_compact_list) {
+                    if (history.kind == libbitcoin::chain::point_kind::output &&
+                        history.height >= start_height && history.height <= end_height) {
+                        //It's an output
+                        boost::latch latch2(2);
+                        //Fetch txn to get the blockindex and height
+                        chain.fetch_transaction(history.point.hash(), false,
+                                                [&](const libbitcoin::code &ec,
+                                                    libbitcoin::transaction_const_ptr tx_ptr, size_t index,
+                                                    size_t height) {
+                                                    if (ec == libbitcoin::error::success) {
+                                                        if (height >= start_height && height <= end_height) {
+                                                            json_object[i]["txid"] = libbitcoin::encode_hash(
+                                                                    history.point.hash());
+                                                            json_object[i]["index"] = history.point.index();
+                                                            json_object[i]["address"] = address.encoded();
+                                                            json_object[i]["blockindex"] = index;
+                                                            json_object[i]["height"] = height;
+                                                            json_object[i]["satoshis"] = std::to_string(history.value);
+                                                            ++i;
                                                         }
-                                                        latch2.count_down();
-                                                    });
-                            latch2.count_down_and_wait();
-                        }
+                                                    } else {
+                                                        std::cout << "Tx not found" << std::endl;
+                                                    }
+                                                    latch2.count_down();
+                                                });
+                        latch2.count_down_and_wait();
 
-                        // Check if it was spent
-                        {
-                            boost::latch latch2(2);
-                            chain.fetch_spend(history.output, [&](const libbitcoin::code &ec,
-                                                                  libbitcoin::chain::input_point input) {
-                                if (ec == libbitcoin::error::success) {
-                                    // TODO check height without calling fetch_transaction
-                                    boost::latch latch3(2);
-                                    chain.fetch_transaction(input.hash(), false,
-                                                            [&](const libbitcoin::code &ec,
-                                                                libbitcoin::transaction_const_ptr tx_ptr,
-                                                                size_t index,
-                                                                size_t height) {
-                                                                if (ec == libbitcoin::error::success) {
-                                                                    if (height >= start_height &&
-                                                                        height <= end_height) {
-                                                                        json_object[i]["txid"] = libbitcoin::encode_hash(
-                                                                                input.hash());
-                                                                        json_object[i]["index"] = input.index();
-                                                                        json_object[i]["address"] = address.encoded();
-                                                                        json_object[i]["blockindex"] = index;
-                                                                        json_object[i]["height"] = height;
-                                                                        json_object[i]["satoshis"] =
-                                                                                "-" + std::to_string(history.value);
-                                                                        ++i;
-                                                                    } else {
-                                                                        // Do nothing out of limits
-                                                                        std::cout << "\tInput > height"
-                                                                                  << std::endl;
-                                                                    }
+                        //Check if it was spent and get the txn data
+                        boost::latch latch3(2);
+                        chain.fetch_spend(history.point, [&](const libbitcoin::code &ec,
+                                                             libbitcoin::chain::input_point input) {
+                            if (ec == libbitcoin::error::success) {
+                                boost::latch latch4(2);
+                                chain.fetch_transaction(input.hash(), false,
+                                                        [&](const libbitcoin::code &ec,
+                                                            libbitcoin::transaction_const_ptr tx_ptr,
+                                                            size_t index,
+                                                            size_t height) {
+                                                            if (ec == libbitcoin::error::success) {
+                                                                if (height >= start_height &&
+                                                                    height <= end_height) {
+                                                                    json_object[i]["txid"] = libbitcoin::encode_hash(
+                                                                            input.hash());
+                                                                    json_object[i]["index"] = input.index();
+                                                                    json_object[i]["address"] = address.encoded();
+                                                                    json_object[i]["blockindex"] = index;
+                                                                    json_object[i]["height"] = height;
+                                                                    json_object[i]["satoshis"] =
+                                                                            "-" + std::to_string(history.value);
+                                                                    ++i;
                                                                 } else {
-                                                                    std::cout << "Tx not found" << std::endl;
+                                                                    // Do nothing out of limits
+                                                                    std::cout << "\tInput > height"
+                                                                              << std::endl;
                                                                 }
-                                                                latch3.count_down();
-                                                            });
-                                    latch3.count_down_and_wait();
-                                }
-                                latch2.count_down();
-                            });
-                            latch2.count_down_and_wait();
-                        }
+                                                            } else {
+                                                                std::cout << "Tx not found" << std::endl;
+                                                            }
+                                                            latch4.count_down();
+                                                        });
+                                latch4.count_down_and_wait();
+                            }
+                            latch3.count_down();
+                        });
+                        latch3.count_down_and_wait();
                     }
                 }
             } else {
